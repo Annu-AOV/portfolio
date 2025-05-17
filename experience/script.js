a9c7c9c418c969a45153be5f196995d52cec0e14
Annu <annu.apnaonlinevyapar@gmail.com>
2025-05-17 14:30:46 +0530

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Annu Soni";
         $("#favicon").attr("href","/assets/images/pf-fav.jpg");
     }
     else {
